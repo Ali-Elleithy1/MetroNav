fd05b8f6891526a822f738d69ff84ae1582a9ca0
alik13 <ali.kh1615@gmail.com>
2022-04-17 00:29:27 +0200

    Add navigation dependency

--- app/src/main/java/com/example/metronav/MainActivity.kt
+++ app/src/main/java/com/example/metronav/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.metronav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.metronav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val dataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
     }
 }
