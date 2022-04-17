56335da4364eb1c71555ae25b20c9f0969f68c54
alik13 <ali.kh1615@gmail.com>
2022-04-17 03:57:15 +0200

    Add navigation graph and drawer layout

--- app/src/main/java/com/example/metronav/MainActivity.kt
+++ app/src/main/java/com/example/metronav/MainActivity.kt
@@ -3,12 +3,40 @@ package com.example.metronav
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.metronav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView,navController)
 
+        navController.addOnDestinationChangedListener{nc: NavController,nd: NavDestination,args:Bundle? ->
+            if(nd.id == nc.graph.startDestinationId){
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
