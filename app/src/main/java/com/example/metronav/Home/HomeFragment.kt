7d52ce687bab172af2b542c51322a954ea79bcfe
alik13 <ali.kh1615@gmail.com>
2022-04-19 15:56:07 +0200

    Add view model for home fragment and move them into a new directory.

--- app/src/main/java/com/example/metronav/Home/HomeFragment.kt
+++ app/src/main/java/com/example/metronav/Home/HomeFragment.kt
@@ -1,10 +1,13 @@
-package com.example.metronav
+package com.example.metronav.Home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.metronav.R
+import com.example.metronav.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +20,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding:FragmentHomeBinding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_home,container,false)
+
+        return binding.root
     }
 }
---
+++ app/src/main/java/com/example/metronav/Home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.metronav.Home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel :ViewModel(){
+
+}
