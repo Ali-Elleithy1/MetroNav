60a81c2187fb085ed844248c229bb82c98c1b373
alik13 <ali.kh1615@gmail.com>
2022-04-19 20:45:27 +0200

    Add array adapter for the AutoCompleteTextView

--- app/src/main/java/com/example/metronav/Home/HomeFragment.kt
+++ app/src/main/java/com/example/metronav/Home/HomeFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.metronav.R
+import com.example.metronav.Station
 import com.example.metronav.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var viewModel:HomeViewModel
+    private lateinit var viewModelFactory: HomeViewModelFactory
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+
+    var list = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +30,25 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding:FragmentHomeBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_home,container,false)
+
+        viewModelFactory = HomeViewModelFactory(binding.fromACTV.toString(),binding.toACTV.toString())
+        viewModel = ViewModelProvider(this,viewModelFactory).get(HomeViewModel::class.java)
+
+        for (item in viewModel.stations)
+        {
+            list.add(item.name)
+        }
+
+        //Create Array Adapter for AutoCompleteTextView
+        arrayAdapter = ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1,list)
+        binding.fromACTV.setAdapter(arrayAdapter)
+        binding.toACTV.setAdapter(arrayAdapter)
+
+
+
+        binding.findRouteButton.setOnClickListener {
+
+        }
 
         return binding.root
     }
