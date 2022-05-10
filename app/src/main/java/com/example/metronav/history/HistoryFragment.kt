145ad05e910ccce0c4c12bfd3d6dc14941b7ab2a
alik13 <ali.kh1615@gmail.com>
2022-05-10 21:08:17 +0200

    Move into new directory

--- app/src/main/java/com/example/metronav/history/HistoryFragment.kt
+++ app/src/main/java/com/example/metronav/history/HistoryFragment.kt
@@ -1,11 +1,11 @@
-package com.example.metronav
+package com.example.metronav.history
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import com.example.metronav.R
 
 class HistoryFragment : Fragment() {
 
