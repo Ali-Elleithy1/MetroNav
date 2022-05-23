d7c8275c4f28f0a41e2fdc9335e6ee5dc3677121
alik13 <ali.kh1615@gmail.com>
2022-05-24 00:24:09 +0200

    Add variables to connect to view model and database

--- app/src/main/java/com/example/metronav/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/metronav/registration/RegistrationFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.metronav.R
+import com.example.metronav.database.MetroDatabase
+import com.example.metronav.databinding.FragmentRegistrationBinding
 
 
 class RegistrationFragment : Fragment() {
@@ -17,8 +22,27 @@ class RegistrationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        val binding: FragmentRegistrationBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_registration,container,false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = MetroDatabase.getInstance(application).userDatabaseDao
+
+        val viewModelFactory = RegistrationViewModelFactory(dataSource,application)
+
+        val viewModel = ViewModelProvider(this,viewModelFactory).get(RegistrationViewModel::class.java)
+
+        binding.registrationViewModel = viewModel
+
+        binding.lifecycleOwner = this
+
+        binding.registerButton.setOnClickListener { view ->
+            viewModel.onRegister(binding.fnameEdit.text.toString(), binding.lnameEdit.text.toString(), binding.rEmailEdit.text.toString(), binding.rPasswordEdit.text.toString())
+            view.findNavController().navigate(R.id.action_registrationFragment_to_loginFragment)
+        }
+
+        return binding.root
     }
 }
